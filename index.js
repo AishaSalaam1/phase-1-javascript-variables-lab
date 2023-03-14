2fe99af619f451c8f8aa34e2b1ce47d98f00e064
Aisha Hussein <aishasalaam07@gmail.com>
2023-03-14 03:52:31 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+ 
+      const companyName =('Scuber');
+    
+    console.log(companyName);
+
+    // const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = "Chelsea";
+
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = "Susan Smith";
+console.log(companyCeo);
